67027e7335fe0708f4be5a9b0d9bccca2cb73c47
Devashish Shrestha <shresthdevashish@gmail.com>
2025-03-29 15:36:11 -0700

    Initial Flask app setup

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Hello World")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    port = 5000
+    print(f"Starting server on port {port}")
+    app.run(debug=True, port=port)
